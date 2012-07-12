6ac2ab0c48c79214e41167b27656f49b358bc931
Christoph Heer <Christoph.Heer@gmail.com>
2012-07-12 21:09:49 +0200

    Add more install requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# flacker
+# Copyright: (c) 2012 Christoph Heer
+# License: BSD
+
+
 from setuptools import setup
 
 setup(
@@ -11,7 +17,9 @@ setup(
     packages=['flacker'],
     zip_safe=False,
     install_requires=[
-        'flask==0.9',
-        'flask-script',
+        'Flask==0.9',
+        'Flask-Script==0.3.3',
+        'Flask-And-Redis==0.3.1',
+        'BitTorrent-bencode==5.0.8.1',
     ]
 )
